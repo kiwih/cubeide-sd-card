0d1ac41809630bccefd311d9dfea877eb29bbaed
Tarados <thomas.antoine@live.be>
2023-07-25 18:12:46 +0200

    Improve writing throughput

--- cubeide-sd-card/FATFS/Target/user_diskio_spi.c
+++ cubeide-sd-card/FATFS/Target/user_diskio_spi.c
@@ -132,9 +132,7 @@ void xmit_spi_multi (
 	UINT btx			/* Number of bytes to send (even number) */
 )
 {
-	for(UINT i=0; i<btx; i++) {
-		xchg_spi(*(buff+i));
-	}
+	HAL_SPI_Transmit(&SD_SPI_HANDLE, buff, btx, 100);
 }
 #endif
 
